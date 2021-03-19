ff724442d458e09cf4694ce148a974c01e1c1f6b
damian2121 <nico.damian.ex@gmail.com>
2021-03-18 21:56:53 -0300

    first commit

---
+++ db/database.js
@@ -0,0 +1,21 @@
+import { Client } from 'https://deno.land/x/postgres/mod.ts';
+
+class Database {
+    constructor() {
+        this.connect();
+    }
+
+    async connect() {
+        this.client = new Client({
+            user: 'username',
+            database: 'logrocket_deno',
+            hostname: 'dev.cuobad7fvyvp.us-west-2.rds.amazonaws.com',
+            password: 'password',
+            port: 5432,
+        });
+
+        await this.client.connect();
+    }
+}
+
+export default new Database().client;
